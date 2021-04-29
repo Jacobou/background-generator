ade24aaf06e4463648087595c1bbb23f2eea9973
Jacob <azzizi.m.yakoub@gmail.com>
2021-04-29 20:37:23 +0100

    adding starting project

---
+++ index.js
@@ -0,0 +1,20 @@
+function permutations(string) {
+  
+    let arr = [];
+    let res = [];
+    
+    for(let i = 0; i<string.length; i++){
+      arr.push(string.charAt(i));
+    }
+
+    for(let i = 0; i<arr.length;i++){
+        for(let j = 0; j<arr.length;J++){
+            
+        }
+    }
+
+    console.log(arr);
+  }
+
+
+  permutations('aabb')
